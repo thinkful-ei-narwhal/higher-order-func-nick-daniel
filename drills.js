cbe032555aa8277acfc3e5e4ae1abb3287e3445a
DanielJSottile <danielsottile@gmail.com>
2020-03-16 17:16:17 -0400

    1

---
+++ drills.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// 1
+
+function repeat(fn, n) {
+  for (let i=0; i < n; i++) {
+    fn();
+  }
+}
+
+function hello() {
+  console.log('Hello World');
+}
+
+function goodbye() {
+  console.log('Goodbye World');
+}
+
+repeat(goodbye, 5);
+
+// 2
